4e13e903608984d8c095c856998251255542fb0c
Abeer <abeeralghamdi.jed.k1@tuwaiq.edu.sa>
2021-10-21 01:07:08 +0300

    hw day4

---
+++ app/src/main/java/com/shady/room/recyclerview_item.kt
@@ -0,0 +1,11 @@
+package com.shady.room
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class recyclerview_item : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recyclerview_item)
+    }
+}
