b176f9086b4ec2f6295977b49891ec7c4500e5c3
Basil19 <bojsl-9@hotmail.com>
2021-10-20 08:08:00 +0300

    WEEK4-DAY3->DONE);

--- app/src/main/java/com/shady/room/MainActivity.kt
+++ app/src/main/java/com/shady/room/MainActivity.kt
@@ -2,6 +2,7 @@ package com.shady.room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tuwaiq.roomdb.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
